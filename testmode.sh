d4c6e5ff8d5f6b9d04e2c95c1725b01abcd2e9f4
ashdaily <ashtokyo31@gmail.com>
2024-10-01 09:02:34 +0100

    testmode shell

---
+++ testmode.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+./cleanup.sh
+
+# Check if the log file exists
+LOG_FILE="./src/log_generator/api_requests.log"
+
+if [ -f "$LOG_FILE" ]; then
+    echo "$LOG_FILE exists."
+else
+    echo "$LOG_FILE does not exist. Creating an empty log file."
+    touch "$LOG_FILE"
+    echo "$LOG_FILE has been created."
+fi
+
+echo "Building Docker Compose services..."
+docker compose --env-file .env.local build --no-cache
+
+echo "Starting Docker Compose services..."
+docker compose --env-file .env.local up -d log_processor rated_db rated_api
