c75910f346fe767e171a91f0f5fb08050d3905dd
ashdaily <ashtokyo31@gmail.com>
2024-10-03 03:11:47 +0100

    disable logging for tests

--- src/api/test_customer_api.py
+++ src/api/test_customer_api.py
@@ -1,16 +1,17 @@
 import unittest
 from fastapi.testclient import TestClient
 from main import app
+import logging
 
 
 class TestCustomerAPI(unittest.TestCase):
     def setUp(self):
+        logging.disable(logging.CRITICAL)
         self.client = TestClient(app)
 
     def test_get_customer_stats_success(self):
         # 2024-10-01 20:58:21 cust_1 /api/v1/resource4 400 0.195
         response = self.client.get("/customers/cust_1/stats?from_date=2024-10-01")
-        print(response.json())
         self.assertEqual(response.status_code, 200)
 
     def test_get_customer_stats_fails_on_passing_bad_date(self):
